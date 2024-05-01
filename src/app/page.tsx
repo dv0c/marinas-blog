a0ddf4920d8f86950fd2bd4c5bb57c9dce8dd916
dv0c <tasosmeidanis12@gmail.com>
2024-05-01 09:59:27 +0300

    new file:   public/main-screen.jpg 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx 	modified:   src/components/Navigation.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { Navigation } from "@/components/Navigation";
+import Image from "next/image";
+import Mainscreen from '../../public/main-screen.jpg'
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <div className="space-y-10 p-5 md:p-0">
+        <div className="space-y-3 mx-auto max-w-3xl mt-10">
+          <h1 className="text-3xl font-semibold md:font-normal md:text-5xl">Starter - a Minimalist</h1>
+          <h1 className="text-3xl md:text-5xl font-semibold md:font-normal">Personal Blog Template</h1>
+          <p className="text-muted-foreground text-sm md:text-base max-w-xl font-semibold pt-5">Lorem ipsum dolor. Asperiores accusantium sit consequuntur doloribus blanditiis aliquam laudantium nesciunt illo adipisci voluptatem?</p>
+        </div>
+        <div className="relative overflow-hidden mx-auto max-w-5xl ">
+          <Image src={Mainscreen} className="object-cover w-full rounded-xl h-[50vh] md:h-[70vh]" alt="img" placeholder="blur" width={500} height={500} />
+          <p className="text-muted-foreground relative text-center text-xs md:text-sm p-3">A dock on a lake with a mountain in the background photo – Free Mt. rainier Image on <span className="underline cursor-pointer">Unsplash</span></p>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,8 +18,8 @@ export const Navigation = () => {
   ];
   return (
     <nav>
-      <header className="flex max-w-[100rem] mx-auto py-8 items-center justify-between gap-5">
-        <Link href="/" className="text-2xl font-semibold">
+      <header className="flex px-5 max-w-[100rem] mx-auto py-8 items-center justify-between gap-5">
+        <Link href="/" className="text-xl md:text-2xl font-semibold">
           {process.env.SITE_NAME}
         </Link>
         <ul className="flex items-center gap-5 text-sm">

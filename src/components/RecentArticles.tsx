8f3ba46c8ed7811e90c3faeaddf5f34753886e5d
dv0c <tasosmeidanis12@gmail.com>
2024-05-02 10:44:01 +0300

    modified:   src/components/RecentArticles.tsx

--- src/components/RecentArticles.tsx
+++ src/components/RecentArticles.tsx
@@ -1,9 +1,10 @@
 import { getArticles } from "@/helpers/fetcher"
 import Article from "./Article"
 
+export const revalidate = 1000
+
 const RecentArticles = async () => {
     const posts = await getArticles()
-    console.log(posts);
     
     return <div className="max-w-4xl mx-auto pt-14">
         <h1 className="text-sm md:text-md font-semibold text-muted-foreground">Recent Publications</h1>
